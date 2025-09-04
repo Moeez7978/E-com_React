046b059b5e41bd99c813b9d918b64d1007674aee
moeez7978 <moeez7978911@gmail.com>
2025-09-04 14:16:39 +0500

    Edited Sample Test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,3 +1,28 @@
+beforeAll(() => {
+  const localStorageMock = (() => {
+    let store = {};
+    return {
+      getItem(key) {
+        return store[key] || null;
+      },
+      setItem(key, value) {
+        store[key] = value.toString();
+      },
+      removeItem(key) {
+        delete store[key];
+      },
+      clear() {
+        store = {};
+      },
+    };
+  })();
+
+  Object.defineProperty(window, "localStorage", {
+    value: localStorageMock,
+  });
+});
+
+
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import App from "./App";
